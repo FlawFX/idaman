d26b8119b89e490f2e596a5781f3d0bb7e6b4bac
FlawFX <114559769+FlawFX@users.noreply.github.com>
2022-10-22 15:21:19 +0700

    Update main-menulist.js

--- plugins/main-menulist.js
+++ plugins/main-menulist.js
@@ -567,7 +567,7 @@ let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) % 30
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [ye, ' *Years 🗓️*\n',mo, ' *Month 🌙*\n', d, ' *Days ☀️*\n', h, ' *Hours 🕐*\n', m, ' *Minute ⏰*\n', s, ' *Second ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
+return [ye, ' *Tahun 🗓️*\n',mo, ' *Bulan 🌙*\n', d, ' *Hari ☀️*\n', h, ' *Jam 🕐*\n', m, ' *Menit ⏰*\n', s, ' *Detik ⏱️*'].map(v => v.toString().padStart(2, 0)).join('')
 }
 function ucapan() {
 const time = moment.tz('Asia/Jakarta').format('HH')
